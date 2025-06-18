44508d4fa06081ee8223ef08c201652fffc29c01
stoneshik <strebilly@mail.ru>
2025-06-19 01:22:00 +0300

    Дописал тесты

--- src/test/java/grouper/ParserFromBufferedReaderTest.java
+++ src/test/java/grouper/ParserFromBufferedReaderTest.java
@@ -1,13 +1,16 @@
 package grouper;
 
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -20,7 +23,6 @@ import grouper.parser.Parser;
 import grouper.parser.ParserFromBufferedReader;
 
 class ParserFromBufferedReaderTest {
-    /* Тесты на общую работоспособность парсера без специфики задания */
     @Test
     @DisplayName("Парсинг пустой строки")
     void testParseEmptyReader() throws IOException {
@@ -35,7 +37,7 @@ class ParserFromBufferedReaderTest {
     @Test
     @DisplayName("Парсинг одной строки")
     void testParseSingleLine() throws IOException {
-        final String testLine = "test line";
+        String testLine = "\"A1\"";
         BufferedReader reader = new BufferedReader(new StringReader(testLine));
         Parser parser = new ParserFromBufferedReader(reader);
         ImmutableParseResult parseResult = parser.parse();
@@ -56,7 +58,7 @@ class ParserFromBufferedReaderTest {
     @Test
     @DisplayName("Парсинг 3 строк")
     void testParseMultipleLines() throws IOException {
-        String input = "line1\nline2\nline3";
+        String input = "\"A1\"\n\"A2\"\n\"A3\"";
         BufferedReader reader = new BufferedReader(new StringReader(input));
         Parser parser = new ParserFromBufferedReader(reader);
         ImmutableParseResult parseResult = parser.parse();
@@ -65,27 +67,21 @@ class ParserFromBufferedReaderTest {
         Map<Integer, Map<String, Set<Integer>>> columnValueToRowIds = parseResult.columnValueToRowIds();
 
         assertEquals(3, rows.size());
-        assertEquals(1, rows.get(0).length);
-        assertEquals("line1", rows.get(0)[0]);
-        assertEquals(1, rows.get(1).length);
-        assertEquals("line2", rows.get(1)[0]);
-        assertEquals(1, rows.get(2).length);
-        assertEquals("line3", rows.get(2)[0]);
+        assertArrayEquals(new String[]{"\"A1\""}, rows.get(0));
+        assertArrayEquals(new String[]{"\"A2\""}, rows.get(1));
+        assertArrayEquals(new String[]{"\"A3\""}, rows.get(2));
 
         assertEquals(1, columnValueToRowIds.size());
         assertEquals(3, columnValueToRowIds.get(0).size());
-        assertEquals(1, columnValueToRowIds.get(0).get("line1").size());
-        assertTrue(columnValueToRowIds.get(0).get("line1").contains(0));
-        assertEquals(1, columnValueToRowIds.get(0).get("line2").size());
-        assertTrue(columnValueToRowIds.get(0).get("line2").contains(1));
-        assertEquals(1, columnValueToRowIds.get(0).get("line3").size());
-        assertTrue(columnValueToRowIds.get(0).get("line3").contains(2));
+        assertIterableEquals(new HashSet<>(List.of(0)), columnValueToRowIds.get(0).get("\"A1\""));
+        assertIterableEquals(new HashSet<>(List.of(1)), columnValueToRowIds.get(0).get("\"A2\""));
+        assertIterableEquals(new HashSet<>(List.of(2)), columnValueToRowIds.get(0).get("\"A3\""));
     }
 
     @Test
     @DisplayName("Парсинг с закрытым BufferedReader")
     void testParseWithClosedReader() throws IOException {
-        BufferedReader reader = new BufferedReader(new StringReader("test"));
+        BufferedReader reader = new BufferedReader(new StringReader("\"A1\""));
         reader.close();
         Parser parser = new ParserFromBufferedReader(reader);
 
@@ -95,7 +91,7 @@ class ParserFromBufferedReaderTest {
     @Test
     @DisplayName("Парсинг 3 строк с пробелами")
     void testParseWithWhitespaceLines() throws IOException {
-        String input = "  line1  \n\t\tline2\t\n   line3   ";
+        String input = "  \"A1\"  \n\t\t\"A2\"\t\n   \"A3\"   ";
         BufferedReader reader = new BufferedReader(new StringReader(input));
 
         Parser parser = new ParserFromBufferedReader(reader);
@@ -105,21 +101,15 @@ class ParserFromBufferedReaderTest {
         Map<Integer, Map<String, Set<Integer>>> columnValueToRowIds = parseResult.columnValueToRowIds();
 
         assertEquals(3, rows.size());
-        assertEquals(1, rows.get(0).length);
-        assertEquals("line1", rows.get(0)[0]);
-        assertEquals(1, rows.get(1).length);
-        assertEquals("line2", rows.get(1)[0]);
-        assertEquals(1, rows.get(2).length);
-        assertEquals("line3", rows.get(2)[0]);
+        assertArrayEquals(new String[]{"\"A1\""}, rows.get(0));
+        assertArrayEquals(new String[]{"\"A2\""}, rows.get(1));
+        assertArrayEquals(new String[]{"\"A3\""}, rows.get(2));
 
         assertEquals(1, columnValueToRowIds.size());
         assertEquals(3, columnValueToRowIds.get(0).size());
-        assertEquals(1, columnValueToRowIds.get(0).get("line1").size());
-        assertTrue(columnValueToRowIds.get(0).get("line1").contains(0));
-        assertEquals(1, columnValueToRowIds.get(0).get("line2").size());
-        assertTrue(columnValueToRowIds.get(0).get("line2").contains(1));
-        assertEquals(1, columnValueToRowIds.get(0).get("line3").size());
-        assertTrue(columnValueToRowIds.get(0).get("line3").contains(2));
+        assertIterableEquals(new HashSet<>(List.of(0)), columnValueToRowIds.get(0).get("\"A1\""));
+        assertIterableEquals(new HashSet<>(List.of(1)), columnValueToRowIds.get(0).get("\"A2\""));
+        assertIterableEquals(new HashSet<>(List.of(2)), columnValueToRowIds.get(0).get("\"A3\""));
     }
 
     @Test
@@ -129,5 +119,74 @@ class ParserFromBufferedReaderTest {
         assertThrows(NullPointerException.class, parser::parse);
     }
 
-    /* Тесты проверяющие работоспособность парсера с учетом специфики задания */
+    @Test
+    @DisplayName("Парсинг некорректных строк")
+    void testParseIncorrectLines() throws IOException {
+        String input = "\"8383\"200000741652251\"\n\"79855053897\"83100000580443402\";\"200000133000191\"";
+        BufferedReader reader = new BufferedReader(new StringReader(input));
+        Parser parser = new ParserFromBufferedReader(reader);
+        ImmutableParseResult parseResult = parser.parse();
+
+        assertTrue(parseResult.rows().isEmpty());
+        assertTrue(parseResult.columnValueToRowIds().isEmpty());
+    }
+
+    @Test
+    @DisplayName("Парсинг 3 строк с несколькими значениями в каждой строке")
+    void testParseMultipleLinesWithMultipleValues() throws IOException {
+        String input = "\"A1\";\"B1\";\"C1\"\n\"A2\";\"B2\";\"C2\"\n\"A3\";\"B3\"";
+        BufferedReader reader = new BufferedReader(new StringReader(input));
+        Parser parser = new ParserFromBufferedReader(reader);
+        ImmutableParseResult parseResult = parser.parse();
+
+        List<String[]> rows = parseResult.rows();
+        Map<Integer, Map<String, Set<Integer>>> columnValueToRowIds = parseResult.columnValueToRowIds();
+
+        assertEquals(3, rows.size());
+        assertArrayEquals(new String[]{"\"A1\"", "\"B1\"", "\"C1\""}, rows.get(0));
+        assertArrayEquals(new String[]{"\"A2\"", "\"B2\"", "\"C2\""}, rows.get(1));
+        assertArrayEquals(new String[]{"\"A3\"", "\"B3\""}, rows.get(2));
+
+        assertEquals(3, columnValueToRowIds.size());
+        assertEquals(3, columnValueToRowIds.get(0).size());
+        assertIterableEquals(new HashSet<>(List.of(0)), columnValueToRowIds.get(0).get("\"A1\""));
+        assertIterableEquals(new HashSet<>(List.of(1)), columnValueToRowIds.get(0).get("\"A2\""));
+        assertIterableEquals(new HashSet<>(List.of(2)), columnValueToRowIds.get(0).get("\"A3\""));
+        assertEquals(3, columnValueToRowIds.get(1).size());
+        assertIterableEquals(new HashSet<>(List.of(0)), columnValueToRowIds.get(1).get("\"B1\""));
+        assertIterableEquals(new HashSet<>(List.of(1)), columnValueToRowIds.get(1).get("\"B2\""));
+        assertIterableEquals(new HashSet<>(List.of(2)), columnValueToRowIds.get(1).get("\"B3\""));
+        assertEquals(2, columnValueToRowIds.get(2).size());
+        assertIterableEquals(new HashSet<>(List.of(0)), columnValueToRowIds.get(2).get("\"C1\""));
+        assertIterableEquals(new HashSet<>(List.of(1)), columnValueToRowIds.get(2).get("\"C2\""));
+    }
+
+    @Test
+    @DisplayName("Парсинг 3 строк с несколькими значениями в каждой строке, которые образуют несколько групп")
+    void testParseMultipleLinesWithMultipleValuesAndGroups() throws IOException {
+        String input = "\"111\";\"123\";\"222\"\n\"200\";\"123\";\"100\"\n\"300\";\"\";\"100\"";
+        BufferedReader reader = new BufferedReader(new StringReader(input));
+        Parser parser = new ParserFromBufferedReader(reader);
+        ImmutableParseResult parseResult = parser.parse();
+
+        List<String[]> rows = parseResult.rows();
+        Map<Integer, Map<String, Set<Integer>>> columnValueToRowIds = parseResult.columnValueToRowIds();
+
+        assertEquals(3, rows.size());
+        assertArrayEquals(new String[]{"\"111\"", "\"123\"", "\"222\""}, rows.get(0));
+        assertArrayEquals(new String[]{"\"200\"", "\"123\"", "\"100\""}, rows.get(1));
+        assertArrayEquals(new String[]{"\"300\"", "\"\"", "\"100\""}, rows.get(2));
+
+        assertEquals(3, columnValueToRowIds.size());
+        assertEquals(3, columnValueToRowIds.get(0).size());
+        assertIterableEquals(new HashSet<>(List.of(0)), columnValueToRowIds.get(0).get("\"111\""));
+        assertIterableEquals(new HashSet<>(List.of(1)), columnValueToRowIds.get(0).get("\"200\""));
+        assertIterableEquals(new HashSet<>(List.of(2)), columnValueToRowIds.get(0).get("\"300\""));
+        assertEquals(2, columnValueToRowIds.get(1).size());
+        assertIterableEquals(new HashSet<>(List.of(0, 1)), columnValueToRowIds.get(1).get("\"123\""));
+        assertIterableEquals(new HashSet<>(List.of(2)), columnValueToRowIds.get(1).get("\"\""));
+        assertEquals(2, columnValueToRowIds.get(2).size());
+        assertIterableEquals(new HashSet<>(List.of(0)), columnValueToRowIds.get(2).get("\"222\""));
+        assertIterableEquals(new HashSet<>(List.of(1, 2)), columnValueToRowIds.get(2).get("\"100\""));
+    }
 }
